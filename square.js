33be15f618bae01e9b24c8d7ca7b9ee2de00b14a
sriram000 <srirammani14@gmail.com>
2021-05-28 12:53:31 +0530

    Introduced functions.

--- square.js
+++ square.js
@@ -1,5 +1,3 @@
-const square = (arr) => {
-
 var firstLine = function (arr) {
   var k = '';
   for (var i = 0; i < arr.length; i++) {
@@ -7,40 +5,51 @@ var firstLine = function (arr) {
   }
   return k;
 };
-console.log(firstLine(arr));
 
-var spaceCount = function (space) {
-  var k = '';
-  for (var i = 0; i < space; i++) {
-    k = k + ' ';
-  }
-  return k;
-};
-
-var charac = function (val) {
-  var k = '';
-  for (var j = val; j > 0; j--) {
-    k = k + arr[j - 1];
-    return k;
-  }
-};
-
-var m = arr.length - 1;
-var count = arr.length - 2;
-var spaces = spaceCount(count);
-
-for (var i = 0; i < count; i++) {
-  var firstCharacter = arr[i + 1];
-
-  var secondCharacter = charac(m - i);
-  console.log(firstCharacter + spaces + secondCharacter);
+const lastLine = (arr) => {
+	var m = '';
+	for (var i = arr.length; i > 0; i--){
+		m = m + arr[i - 1];
+	}
+	console.log(m);
 }
-var m = '';
-for (var i = arr.length; i > 0; i--) {
-  m = m + arr[i - 1];
+
+const middleLines = (arr) => {
+	var spaceCount = function (space) {
+		var k = '';
+		for (var i = 0; i < space; i++) {
+			k = k + ' ';
+		}
+		return k;
+	};
+
+	var charac = function (val) {
+		var k = '';
+		for (var j = val; j > 0; j--) {
+			k = k + arr[j - 1];
+			return k;
+		}
+	};
+
+	var m = arr.length - 1;
+	var count = arr.length - 2;
+	var spaces = spaceCount(count);
+
+	for (var i = 0; i < count; i++) {
+		var firstCharacter = arr[i + 1];
+
+		var secondCharacter = charac(m - i);
+		console.log(firstCharacter + spaces + secondCharacter);
+	}
+
 }
-console.log(m);
+
+const square = (arr) => {
+	console.log(firstLine(arr));
+	middleLines(arr);
+	lastLine(arr);
 }
+
 
 square(['a', 'b']);
 square(['a', 'b', 'c']);
