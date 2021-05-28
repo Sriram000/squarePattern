9e017e2d1ddc0b4cc38d4675a52e01f5ddf0c192
sriram000 <srirammani14@gmail.com>
2021-05-28 16:29:17 +0530

    Compressed as one-liner.

--- square.js
+++ square.js
@@ -1,17 +1,9 @@
-const square = (arr) => {
-	const firstLine = arr.join('');
-	const middleLines = arr.slice(1, -1).map((val, i, arr) =>
-		val + ' '.repeat(arr.length) + arr[arr.length - i - 1] );
-	const lastLine = arr.slice().reverse().join('');
-
-	const lines = [
-		firstLine,
-		...middleLines,
-		lastLine,
-	];
-
-	return lines.join('\n');
-}
+const square = (arr) => [
+	arr.join(''),
+	...arr.slice(1, -1).map((val, i, arr) =>
+		val + ' '.repeat(arr.length) + arr[arr.length - i - 1] ),
+	arr.slice().reverse().join(''),
+].join('\n');
 
 console.log(square(['a', 'b']));
 console.log(square(['a', 'b', 'c']));
