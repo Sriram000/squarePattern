d877636a0f02f6f48ee2eadf24c284b45e8759f6
sriram000 <srirammani14@gmail.com>
2021-05-28 16:39:39 +0530

    Refactored for readability.

--- square.js
+++ square.js
@@ -1,8 +1,16 @@
+const firstLine = (arr) => arr.join('');
+
+const middleLine = (val, i, arr) =>
+	val + ' '.repeat(arr.length) + arr[arr.length - i - 1];
+
+const middleLines = (arr) => arr.slice(1, -1).map(middleLine);
+
+const lastLine = (arr) => arr.slice().reverse().join('');
+
 const square = (arr) => [
-	arr.join(''),
-	...arr.slice(1, -1).map((val, i, arr) =>
-		val + ' '.repeat(arr.length) + arr[arr.length - i - 1] ),
-	arr.slice().reverse().join(''),
+	firstLine(arr),
+	...middleLines(arr),
+	lastLine(arr),
 ].join('\n');
 
 console.log(square(['a', 'b']));
