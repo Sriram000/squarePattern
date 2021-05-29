a85a1d12adf117cce4fb5563d0ba271a3d4c71f8
sriram000 <srirammani14@gmail.com>
2021-05-29 10:23:20 +0530

    Imported diamond.js.

---
+++ diamond.js
@@ -0,0 +1,29 @@
+var repeat = function (char, length) {
+  var m = '';
+  var n = char;
+  for (var i = 0; i < length; i++) {
+    m = m + n;
+  }
+  return m;
+};
+repeat('a', 5);
+var diamond = function () {
+  var arr = ['a', 'b', 'c', 'd'];
+  var columns = arr.length * 2 - 1;
+
+  for (var i = 0; i < arr.length; i++) {
+    var n = 2 * i + 1;
+    var spaceCount = (columns - n) / 2;
+    var spaces = repeat(' ', spaceCount);
+    var characters = repeat(arr[i], 2 * i + 1);
+    console.log(spaces + characters + spaces);
+  }
+  for (var i = arr.length - 1; i > 0; i--) {
+    var n = 2 * i - 1;
+    var spaceCount = (columns - n) / 2;
+    var spaces = repeat(' ', spaceCount);
+    var characters = repeat(arr[i - 1], 2 * i - 1);
+    console.log(spaces + characters + spaces);
+  }
+};
+diamond();
