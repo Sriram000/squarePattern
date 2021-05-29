2d8ff95952ed512c5718dbd05c089526830dc86c
sriram000 <srirammani14@gmail.com>
2021-05-29 12:14:45 +0530

    Split the functions.

--- diamond.js
+++ diamond.js
@@ -1,5 +1,3 @@
-const diamondPattern = (arr) => {
-
 var repeat = function (char, length) {
   var m = '';
   var n = char;
@@ -9,28 +7,32 @@ var repeat = function (char, length) {
   return m;
 };
 
-var diamond = function () {
+var diamond = function (arr) {
 
   var columns = arr.length * 2 - 1;
+	var result = '';
 
   for (var i = 0; i < arr.length; i++) {
     var n = 2 * i + 1;
     var spaceCount = (columns - n) / 2;
     var spaces = repeat(' ', spaceCount);
     var characters = repeat(arr[i], 2 * i + 1);
-    console.log(spaces + characters + spaces);
+    result += spaces + characters + spaces + '\n';
   }
   for (var i = arr.length - 1; i > 0; i--) {
     var n = 2 * i - 1;
     var spaceCount = (columns - n) / 2;
     var spaces = repeat(' ', spaceCount);
     var characters = repeat(arr[i - 1], 2 * i - 1);
-    console.log(spaces + characters + spaces);
+    result += spaces + characters + spaces + '\n';
   }
-};
-return diamond();
+	return result;
 }
 
-diamondPattern(['a', 'b']);
-diamondPattern(['a', 'b', 'c']);
-diamondPattern(['a', 'b', 'c', 'd', 'e']);
+const diamondPattern = (arr) => {
+	return diamond(arr);
+}
+
+console.log(diamondPattern(['a', 'b']));
+console.log(diamondPattern(['a', 'b', 'c']));
+console.log(diamondPattern(['a', 'b', 'c', 'd', 'e']));
