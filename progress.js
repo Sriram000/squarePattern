17fcfaff582a483c19c846879531bc4fa0ab3dc4
sriram000 <srirammani14@gmail.com>
2021-05-31 15:29:03 +0530

    Printed mirror image.

--- progress.js
+++ progress.js
@@ -1,9 +1,16 @@
 const diamond = function (arr) {
 	let result = '';
+	const midPoint = arr.length;
+	const count = midPoint * 2 - 1;
 
-	for(let i = 0; i < arr.length; i++) {
-		result += arr[i] + '\n';
+	for(let i = 1; i <= count; i++) {
+		const distance = Math.abs(midPoint - i);
+		const charPos = midPoint - distance;
+		const charIndex = charPos - 1;
+
+		result += arr[charIndex] + '\n';
 	}
+
 	return result;
 }
 
